7894a110a4ae51af7e4a555c279c89655431971b
Ana Nikolova <ani.nik19@gmail.com>
2019-12-03 06:58:17 +0100

    passing down props to components

--- src/features/event/EventList/EventListAttendee.js
+++ src/features/event/EventList/EventListAttendee.js
@@ -3,15 +3,11 @@ import { List, Image } from "semantic-ui-react";
 
 class EventListAttendee extends Component {
   render() {
-    const randomUserImg = () => {
-      const num = Math.round(Math.random() * 98);
-      const MaleUser = `https://randomuser.me/api/portraits/men/${num}.jpg`;
-      const femaleUser = `https://randomuser.me/api/portraits/women/${num}.jpg`;
-      return Math.round(Math.random()) ? MaleUser : femaleUser;
-    };
+    const { attendee } = this.props;
+
     return (
       <List.Item>
-        <Image as="a" size="mini" circular src={randomUserImg()} />
+        <Image as="a" size="mini" circular src={attendee.photoURL} />
       </List.Item>
     );
   }
--- src/features/event/EventList/EventListItem.js
+++ src/features/event/EventList/EventListItem.js
@@ -4,28 +4,18 @@ import EventListAttendee from "./EventListAttendee";
 
 class EventListItem extends Component {
   render() {
-    const randomUserImg = () => {
-      const num = Math.round(Math.random() * 98);
-      const MaleUser = `https://randomuser.me/api/portraits/men/${num}.jpg`;
-      const femaleUser = `https://randomuser.me/api/portraits/women/${num}.jpg`;
-      return Math.round(Math.random()) ? MaleUser : femaleUser;
-    };
+    const { event } = this.props;
     return (
       <div>
         <Segment.Group style={{ marginBottom: "1.2rem" }}>
           <Segment>
             <Item.Group>
               <Item>
-                <Item.Image
-                  size="tiny"
-                  circular
-                  //src="https://randomuser.me/api/portraits/women/42.jpg"
-                  src={randomUserImg()}
-                />
+                <Item.Image size="tiny" circular src={event.hostPhotoURL} />
                 <Item.Content>
-                  <Item.Header as="a">Event Title</Item.Header>
+                  <Item.Header as="a">{event.title}</Item.Header>
                   <Item.Description>
-                    Hosted by <a>hosted by</a>
+                    Hosted by <a>{event.hostedBy}</a>
                   </Item.Description>
                 </Item.Content>
               </Item>
@@ -33,19 +23,19 @@ class EventListItem extends Component {
           </Segment>
           <Segment>
             <span>
-              <Icon name="clock" /> date |
-              <Icon name="marker" /> time
+              <Icon name="clock" /> {event.date} |
+              <Icon name="marker" /> {event.venue}
             </span>
           </Segment>
           <Segment secondary>
             <List horizontal>
-              <EventListAttendee />
-              <EventListAttendee />
-              <EventListAttendee />
+              {event.attendees.map(attendee => (
+                <EventListAttendee key={attendee.id} attendee={attendee} />
+              ))}
             </List>
           </Segment>
           <Segment clearing>
-            <span>Description will go here</span>
+            <span>{event.description}</span>
             <Button as="a" color="teal" floated="right" content="View" />
           </Segment>
         </Segment.Group>
