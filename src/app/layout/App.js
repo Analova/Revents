54c23c73b8093c85ac65396935f7061299f7f888
Ana Nikolova <ani.nik19@gmail.com>
2019-12-08 16:40:18 +0100

    EventDetailedSideBar, EventDetailedChat and EventDetailedInfo ui done

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar/NavBar";
 import { Container } from "semantic-ui-react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import EventDetailedPage from "../../features/event/EventDetails/EventDetailedPage";
 import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
@@ -23,7 +23,7 @@ class App extends Component {
             <Fragment>
               <NavBar />
               <Container className="main">
-                <Route path="/events" component={EventDashboard} />
+                <Route exact path="/events" component={EventDashboard} />
                 <Route path="/events/:id" component={EventDetailedPage} />
                 <Route path="/people" component={PeopleDashboard} />
                 <Route path="/pofile/:id" component={UserDetailedPage} />
